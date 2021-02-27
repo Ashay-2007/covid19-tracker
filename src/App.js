cd47853a9971102fb1fb695bfd529be4173ca6b6
Ashay2007 <ashaywanjarkar123@gmail.com>
2021-02-27 23:40:50 +0530

    added header and dropdown list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,66 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import {FormControl, Select, MenuItem} from '@material-ui/core';
+
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) =>  response.json())
+      .then((data) => {
+        const countries = data.map(country => (
+          {
+            name: country.country,
+            value: country.countryInfo.iso2
+          }
+        ));
+        setCountries(countries);
+      });
+    };
+
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  }
+
   return (
     <div className="app">
-      <h1>Let's build a covid19 tracker. Hello Developers!</h1>
+      
+      {/* Header */} {/* Title + Select input dropdown field */}
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {/* Loop through all the countries and show a drop down list */}
+            {
+              countries.map(country => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))
+            }
 
-      {/* Header */}
-      {/* Title + Select input dropdown field */}
+          </Select>
+        </FormControl>
+      </div>
+      
 
+      
+     
       {/* Infograph */}
       {/* Infograph */}
       {/* Infograph */}
 
       {/* Table */}
       {/* Graph */}
-      
+
       {/* Map */}
 
     </div>
